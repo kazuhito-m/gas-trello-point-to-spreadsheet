46f8839903e4b19140de85104b8f5fafa46d6961
kazuhito_m <kazuhito.sumpic@gmail.com>
2019-09-05 01:03:08 +0900

    implement: テストやデプロイに対応するため、npmの概念(package.json)と、ある程度のライブラリを導入。

---
+++ tests/domain/model/googlespread/CellPosition.spec.ts
@@ -0,0 +1,8 @@
+import CellPosition from "../../../../src/domain/model/googlespread/CellPosition";
+
+describe('CellPosition のテスト.', () => {
+  it('SpreadSheetのcolumn,rowから、絶対参照方式の文字列(A1 など)に変換できる。', () => {
+    expect('A1').toEqual(new CellPosition(0, 0).adderssKey());
+    expect('E2').toEqual(new CellPosition(3, 4).adderssKey());
+  });
+});
