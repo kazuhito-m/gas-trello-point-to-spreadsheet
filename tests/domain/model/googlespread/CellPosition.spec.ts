f17fd871422186cff166d51a5765ce1113b17f2f
kazuhito_m <kazuhito.sumpic@gmail.com>
2022-12-16 11:49:03 +0900

    debug: 通るテストに戻す。

--- tests/domain/model/googlespread/CellPosition.spec.ts
+++ tests/domain/model/googlespread/CellPosition.spec.ts
@@ -2,7 +2,7 @@ import CellPosition from "../../../../src/domain/model/googlespread/CellPosition
 
 describe('CellPosition のテスト.', () => {
   it('SpreadSheetのcolumn,rowから、絶対参照方式の文字列(A1 など)に変換できる。', () => {
-    expect('A2').toEqual(new CellPosition(0, 0).adderssKey());
+    expect('A1').toEqual(new CellPosition(0, 0).adderssKey());
     expect('E4').toEqual(new CellPosition(3, 4).adderssKey());
   });
 });
